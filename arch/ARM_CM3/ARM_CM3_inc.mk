cc6cbdf8d1c8369c1bd3f37ac026f25a77fe6856
Taras Chornenkiy <taraschrt06@gmail.com>
2016-10-06 18:58:44 +0300

    Modules cmake files are ready. Issue with the main one

--- arch/ARM_CM3/ARM_CM3_inc.mk
+++
@@ -1,5 +0,0 @@
-##############################################################
-#   Describes include directories and files of the module    #
-##############################################################
-
-ARM_CM3_INCLUDE_DIRS := $(PROJECT_DIRECTORY)/arch/ARM_CM3/inc
